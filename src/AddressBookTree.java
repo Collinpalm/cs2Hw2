92cc24195398d348dba87e4b36648e956b0bcfcc
Collin Palm <collinpalm@gmail.com>
2021-10-25 16:04:32 -0400

    starting node class

---
+++ src/AddressBookTree.java
@@ -0,0 +1,11 @@
+public class AddressBookTree {
+}
+
+class Node<T, U> {
+    private T name;
+    private U office;
+    public Node(T nameInput, U officeInput){
+        name = nameInput;
+        office = officeInput;
+    }
+}
